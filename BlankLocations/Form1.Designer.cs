69fb7fbd3bc4631e1f7d5509ddc8838f365b519b
Ben Scattergood <ben.s1234@hotmail.co.uk>
2022-06-14 15:54:31 +0100

    Init

--- BlankLocations/Form1.Designer.cs
+++ BlankLocations/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRunReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRunReport
+            // 
+            this.btnRunReport.Location = new System.Drawing.Point(314, 188);
+            this.btnRunReport.Name = "btnRunReport";
+            this.btnRunReport.Size = new System.Drawing.Size(113, 35);
+            this.btnRunReport.TabIndex = 0;
+            this.btnRunReport.Text = "Run Report";
+            this.btnRunReport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRunReport);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRunReport;
     }
 }
 
---
+++ BlankLocations_Tests/G05_Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using BlankLocations;
+
+namespace BlankLocations_Tests
+{
+    [TestClass]
+    public class G05_Tests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            G05.PopulateG05();
+        }
+
+        [TestMethod]
+        public void wsRange_Test()
+        {
+            string[,] temp = G05.wsRange;
+            Assert.AreEqual("11J17", temp[1, 2]);
+            Assert.AreNotEqual("11J16", temp[1, 2]);
+            Assert.AreEqual("VAN.WB65", temp[16140, 0]);
+        }
+
+        [TestMethod]
+        public void ExtractExcelDataIntoLocationsAndBlanks_Test()
+        {
+            Assert.AreEqual(G05.locations["SEAVS7006V206"], "11S01");
+            Assert.AreEqual(G05.blanks[3].Item1, "101330518");
+        }
+
+        [TestMethod]
+        public void CalculateBlankLocationValues_Test()
+        {
+            G05.CalculateBlankLocationValues();
+            Assert.AreEqual("549777551", G05.blanks[7].Item1);
+            Assert.AreEqual("11T18", G05.calculatedBlanks[12].Item3);
+            Assert.AreEqual("11Q01", G05.calculatedBlanks[34].Item3);
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            G05.Cleanup();
+        }
+    }
+}
